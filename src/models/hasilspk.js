e9572c38cde20c37880dc05d085d0a5760e99bd6
Medusaa36 <rajahasbullah06@gmail.com>
2026-04-20 19:59:25 +0700

    update model hasil spk

--- src/models/hasilspk.js
+++ src/models/hasilspk.js
@@ -1,79 +1,85 @@
 import mongoose from "../config/db.js";
 
-const hasilSpk = new mongoose.Schema({
-  nama: {
-    type: String,
-    required: true,
-  },
-  gender: {
-    type: String,
-  },
-  usia: {
-    type: Number,
-  },
-  status: {
-    type: String,
-  },
-  aktivitas: {
-    type: String,
-    required: true,
-  },
+const hasilSpk = new mongoose.Schema(
+  {
+    nama: {
+      type: String,
+      required: true,
+    },
+    gender: String,
+    usia: Number,
+    status: String,
 
-  konsentrasi: {
-    type: Number,
-    required: true,
-    min: 1,
-    max: 5,
-  },
-  konsistensi: {
-    type: Number,
-    required: true,
-    min: 1,
-    max: 5,
-  },
-  durasi: {
-    type: Number,
-    required: true,
-    min: 1,
-    max: 5,
-  },
-  kelelahan: {
-    type: Number,
-    required: true,
-    min: 1,
-    max: 5,
-  },
-  lingkungan: {
-    type: Number,
-    required: true,
-    min: 1,
-    max: 5,
-  },
-  gangguan: {
-    type: Number,
-    required: true,
-    min: 1,
-    max: 5,
-  },
+    aktivitas: {
+      type: String,
+      required: true,
+    },
 
-  skor: {
-    type: Number,
-  },
+    waktu_luang: {
+      type: String,
+      enum: ["pagi", "siang", "sore", "malam", "fleksibel"],
+      required: true,
+    },
 
-  rekomendasi: {
-    kode: Number,
-    judul: String,
-    waktu: String,
-    pola: String,
-    deskripsi: String,
-    tips: [String],
-  },
+    konsentrasi: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    konsistensi: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    durasi: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    kelelahan: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    lingkungan: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    gangguan: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
 
-  createdAt: {
-    type: Date,
-    default: Date.now,
+    skor: {
+      type: Number,
+      min: 0,
+      max: 100,
+      required: true,
+    },
+
+    rekomendasi: {
+      kode: Number,
+      judul: String,
+      waktu: String,
+      pola: String,
+      deskripsi: String,
+      tips: [String],
+      alasan: [String],
+      faktor: [String],
+    },
   },
-});
+  {
+    timestamps: true,
+  },
+);
 
 const hasilSpkCollection = mongoose.model("HasilSpk", hasilSpk);
 
