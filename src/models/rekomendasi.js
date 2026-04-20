4cc2309b5dba1b8efd109ba354c2b256b4289392
Medusaa36 <rajahasbullah06@gmail.com>
2026-04-20 19:59:44 +0700

    update models rekomendasi

--- src/models/rekomendasi.js
+++ src/models/rekomendasi.js
@@ -3,10 +3,12 @@ import mongoose from "../config/db.js";
 const rekomendasiSchema = new mongoose.Schema({
   kode: Number,
   judul: String,
-  waktu: String,
-  pola: String,
-  deskripsi: String,
-  tips: [String],
+
+  waktu_default: String,
+  pola_default: String,
+
+  deskripsi_base: String,
+  tips_base: [String],
 });
 
 const Rekomendasi = mongoose.model(
